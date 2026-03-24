fedcb6f4fc98b1751743160310415fa955793e54
MathijsN <72303334+MathijsN@users.noreply.github.com>
2026-03-24 13:43:48 +0100

    responsive image preview #1

--- public/script.js
+++ public/script.js
@@ -1,1 +1,15 @@
 
+document.getElementById('file').onchange = function (evt) {
+    const [file] = this.files;
+    if (file) {
+        const preview = document.querySelector('.image-preview');
+        const svg = document.querySelector('.picture-icon');
+
+        // Set the src of the image to the local file path
+        preview.src = URL.createObjectURL(file);
+
+        // Show the image and hide the SVG icon
+        preview.style.display = 'block';
+        svg.style.display = 'none';
+    }
+};
